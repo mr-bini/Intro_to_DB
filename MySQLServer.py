737f5f4b888dfe7b4d945d4d76d4ef1e550d8542
biniyam wondimu <biniyamwondimu12@gmail.com>
2025-08-17 22:35:08 +0300

    MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -14,17 +14,16 @@ def create_database():
         if connection.is_connected():
             cursor = connection.cursor()
             
-            cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+           cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
-        print(f"Error while connecting to MySQL: {e}")
-
-    finally:
-        if cursor is not None:
-            cursor.close()
-        if connection is not None and connection.is_connected():
-            connection.close()
+   except Error as e:
+    print(f"Error while connecting to MySQL: {e}")
+finally:
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
 
 
 if __name__ == "__main__":
