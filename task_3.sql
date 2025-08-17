2f2c44a62c2ec81e39eb56384fbad25d8f75d7c2
biniyam wondimu <biniyamwondimu12@gmail.com>
2025-08-17 22:49:55 +0300

    Update task_3.sql

--- task_3.sql
+++ task_3.sql
@@ -1,2 +1,12 @@
-SHOW TABLES;
+-- task_3.sql
 
+-- Check if the correct database is selected
+SELECT DATABASE() AS current_database;
+IF current_database != 'alx_book_store' THEN
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Incorrect database selected. Please use alx_book_store.';
+END IF;
+
+-- List all tables in the alx_book_store database
+SELECT table_name
+FROM information_schema.tables
+WHERE table_schema = 'alx_book_store';
